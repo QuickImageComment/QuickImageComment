de7cab2220392de5bc60f5fb9c1d824c2a038662
norbertwg <mail@quickimagecomment.de>
2022-05-23 18:12:13 +0200

    adjust number format of file size in details view of file list to format in meta data other

--- QuickImageComment/Utilities/ImageManager.cs
+++ QuickImageComment/Utilities/ImageManager.cs
@@ -137,7 +137,7 @@ namespace QuickImageComment
             {
                 double FileSize = theFileInfo.Length;
                 FileSize = FileSize / 1024;
-                listViewItem.SubItems.Add(FileSize.ToString("N3") + " KB");
+                listViewItem.SubItems.Add(FileSize.ToString("#,### KB"));
                 listViewItem.SubItems.Add(theFileInfo.LastWriteTime.ToString());
                 listViewItem.SubItems.Add(theFileInfo.CreationTime.ToString());
             }
