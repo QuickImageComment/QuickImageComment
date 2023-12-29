1e82cf28ae62c289b726a5f147f4d3bdb696ab97
norbertwg <mail@quickimagecomment.de>
2023-12-29 13:59:55 +0100

    Finalize version 4.57

--- QuickImageComment/Properties/AssemblyInfo.cs
+++ QuickImageComment/Properties/AssemblyInfo.cs
@@ -43,9 +43,9 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.56.*")]
-[assembly: AssemblyInformationalVersion("4.56" + AssemblyInfo.VersionSuffix + AssemblyInfo.Platform + AssemblyInfo.Framework)]
-[assembly: AssemblyFileVersion("4.56.0.0")]
+[assembly: AssemblyVersion("4.57.*")]
+[assembly: AssemblyInformationalVersion("4.57" + AssemblyInfo.VersionSuffix + AssemblyInfo.Platform + AssemblyInfo.Framework)]
+[assembly: AssemblyFileVersion("4.57.0.0")]
 
 class AssemblyInfo
 {
@@ -57,7 +57,7 @@ class AssemblyInfo
     // Version to Check is the last published version
     // for beta versions it is one before AssemblyVersion
     // for released versions it is same as AssemblyVersion
-    public const string VersionToCheck = "4.56";
+    public const string VersionToCheck = "4.57";
 
 #if PLATFORMTARGET_X64
     public const string Platform = " - 64 Bit";
